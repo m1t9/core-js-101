9edeb8ddf9c12b6d3651ceee20eaf84345f2671a
m1t9 <dpleshanov@gmail.com>
2020-12-13 04:08:23 +0300

    add: regex tasks

--- src/05-regex-tasks.js
+++ src/05-regex-tasks.js
@@ -54,7 +54,7 @@ function getRegexForGuid() {
  *
  */
 function getRegexForPitSpot() {
-  throw new Error('Not implemented');
+  return /p.t/;
 }
 
 
@@ -78,8 +78,8 @@ function getRegexForPitSpot() {
  *   'PASSW0RD'.match(validator)  => false
  *   'Pa55'.match(validator) => false
  */
-function getPasswordValidator(/* minLength */) {
-  throw new Error('Not implemented');
+function getPasswordValidator(minLength) {
+  return new RegExp(`^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])[\\da-zA-Z]{${minLength},}$`);
 }
 
 
